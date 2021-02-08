8cdc465320e8144c178916943d2426189124e552
kickscondor <kicks@kickscondor.com>
2021-02-08 23:26:44 +0000

    Merge confact's changes, along with supporting TLS config.

--- src/carbon/adapters/smtp_adapter.cr
+++ src/carbon/adapters/smtp_adapter.cr
@@ -3,37 +3,42 @@ class Carbon::SmtpAdapter < Carbon::Adapter
     setting host : String = "localhost"
     setting port : Int32 = 25
     setting helo_domain : String? = nil
-    setting use_tls : Bool = true
+    setting use_tls : EMail::Client::TLSMode =
+      EMail::Client::TLSMode::STARTTLS
     setting username : String? = nil
     setting password : String? = nil
   end
 
   def deliver_now(email : Carbon::Email)
     auth = get_auth_tuple
+    config = ::EMail::Client::Config.new(settings.host, settings.port,
+      helo_domain: settings.helo_domain)
+    config.use_tls(settings.use_tls)
+    config.use_auth(auth[0], auth[1]) if auth
+    client = ::EMail::Client.new(config)
 
-    ::EMail.send(settings.host, settings.port, auth: auth) do
-      subject email.subject
-
-      from(email.from.address, email.from.name)
+    new_email = ::EMail::Message.new
+    new_email.from email.from.address, email.from.name
 
       email.to.each do |to_address|
-        to(to_address.address, to_address.name)
+      new_email.to(to_address.address, to_address.name)
       end
-
       email.cc.each do |cc_address|
-        cc(cc_address.address, cc_address.name)
+      new_email.cc(cc_address.address, cc_address.name)
       end
-
       email.bcc.each do |bcc_address|
-        bcc(bcc_address.address, bcc_address.name)
+      new_email.bcc(bcc_address.address, bcc_address.name)
       end
 
       email.headers.each do |key, value|
-        custom_header(key, value)
+      new_email.custom_header(key, value)
       end
+    new_email.subject email.subject
+    new_email.message email.text_body
+    new_email.message_html email.html_body
 
-      message email.text_body
-      message_html email.html_body
+    client.start do
+      send(new_email)
     end
   end
 
